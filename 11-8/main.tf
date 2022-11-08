411ede99a9f6aaee273023a2f1254fc0813d7985
senpaikcarter <kennycocopuff@gmail.com>
2022-11-08 10:30:18 -0800

    made several changs to speed up to a valid configuration then adding them all meow, enjoy

--- 11-8/main.tf
+++ 11-8/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
 
 locals {
-  #first_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com"
+  first_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com"
   location= "East US"
 }
 
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "stretch_armstrong" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = var.public_key
+    public_key = local.first_public_key
   }
 
   source_image_reference {
@@ -72,7 +72,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "stretch_armstrong" {
     ip_configuration {
       name      = "internal"
       primary   = true
-      subnet_id = azurerm_subnet.sub_sandwich
+      subnet_id = azurerm_subnet.sub_sandwich.id
     }
   }
 }
