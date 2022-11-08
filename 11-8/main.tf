52a27725d8e79961ae88c2324ab8a9a18aa94772
senpaikcarter <kennycocopuff@gmail.com>
2022-11-08 10:25:08 -0800

    still working towards a better merica, no thats not it more like a better configuration file thats valid

--- 11-8/main.tf
+++ 11-8/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "stretch_armstrong" {
     ip_configuration {
       name      = "internal"
       primary   = true
-      subnet_id = azurerm_subnet.internal.id
+      subnet_id = azurerm_subnet.sub_sandwich
     }
   }
 }
