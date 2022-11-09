c60e01026cb04905edeb35d28dcab5b6596ca05b
senpaikcarter <kennycocopuff@gmail.com>
2022-11-09 10:45:36 -0800

    figured I'd commit after goofin with it so much

--- 11-8/main.tf
+++ 11-8/main.tf
@@ -27,12 +27,36 @@ resource "azurerm_resource_group" "Contaynement" {
 }
 
 resource "azurerm_virtual_network" "darknet" {
-  name                = "example-network"
+  name                = "darknet"
   resource_group_name = azurerm_resource_group.Contaynement.name
   location            = local.location
   address_space       = ["10.0.0.0/16"]
 }
 
+#Network Security Group Starts Here<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
+resource "azurerm_network_security_group" "Julio" {
+  name                = "Julio"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.Contaynement.name
+
+  security_rule {
+    name                       = "donttouchthat"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+#Darknet Subnet's Start Here<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
 resource "azurerm_subnet" "sub_sandwich" {
   name                 = "sub_sandwich"
   resource_group_name  = azurerm_resource_group.Contaynement.name
@@ -140,3 +164,13 @@ resource "azurerm_linux_virtual_machine" "exceptionaldingus" {
 #   description = "this is the output for the IP address"
 #   value = azurerm_linux_virtual_machine_scale_set.reginald.ip
 # }
+
+output "VM-SSH-Key" {
+  description = "The VM Public IP is:"
+  value = azurerm_linux_virtual_machine.exceptionaldingus.admin_ssh_key
+}
+
+output "VM-ID" {
+  description = "The VM ID is:"
+  value = azurerm_linux_virtual_machine_scale_set.reginald.id
+}
