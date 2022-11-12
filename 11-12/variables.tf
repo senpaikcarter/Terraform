8433bc7fc2c21767347236c4a00c4d5eda387ffa
senpaikcarter <kennycocopuff@gmail.com>
2022-11-12 10:32:11 -0800

    creating new project for the dayJ

---
+++ 11-12/variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name_pfx" {
+    description = "VM Names"
+    default = "Virtual-Machine-"
+    type = string 
+}
+
+variable "vm_count" {
+    description = "Number of Virtual Machines"
+    default = 1
+    type = string
+}
